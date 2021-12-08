b093bcf9d684a2a7a666a435b6b0eda21458d31f
victorcomhaire <victor.comhaire@ugent.be>
2021-12-08 15:53:00 +0100

    adding preview code

--- utils/storyblok.js
+++ utils/storyblok.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import StoryblokClient from "storyblok-js-client";
 
 const Storyblok = new StoryblokClient({
-  accessToken: "c1g4yy5Pro9MeJrxV9grQAtt",
+  accessToken: "oBqu2pL6d5alUMuCGB1vowtt",
   cache: {
     clear: "auto",
     type: "memory",
