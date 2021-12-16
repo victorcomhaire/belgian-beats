276dafac5bf55622a02b4755d901f008bdee5d3d
Ward Denolf <denolf.ward@gmail.com>
2021-12-16 19:11:15 +0100

    Top Concerts on front page

--- components/DynamicComponent.js
+++ components/DynamicComponent.js
@@ -30,6 +30,7 @@ import Artist from './Artist'
 import ArtistList from './ArtistList'
 import Song from './Song'
 import SongList from './SongList'
+import TopConcerts from './TopConcerts'
 
 const Components = {
   'teaser': Teaser,
@@ -64,6 +65,7 @@ const Components = {
   'artistlist': ArtistList,
   'song': Song,
   'songlist': SongList,
+  'topconcerts': TopConcerts
 }
 
 
