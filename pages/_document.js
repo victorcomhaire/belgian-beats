bef54a933a50b77f197fe99f545168a71f2e440d
dsantosv <94907533+dsantosv@users.noreply.github.com>
2021-12-16 11:40:11 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,19 +5,16 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
+      
           {* Global Site Tag (gtag.js) - Google Analytics */}
-          {* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=G-3014JHECCB`}
-          />
+          {* <script async src="https://www.googletagmanager.com/gtag/js?id=G-3014JHECCB"/>
           <script
             dangerouslySetInnerHTML={{
               __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3014JHECCB', {
-              page_path: window.location.pathname,
+             window.dataLayer = window.dataLayer || [];
+             function gtag(){dataLayer.push(arguments);}
+             gtag('js', new Date());
+             gtag('config', 'G-3014JHECCB');
             });
           `
             }}
