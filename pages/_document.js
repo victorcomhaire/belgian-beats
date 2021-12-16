db9852b9fd05c081b282e8c0c120b45510ccfdac
dsantosv <diogo.santosvarela@gmail.com>
2021-12-16 16:33:57 +0100

    Tawk.to

--- pages/_document.js
+++ pages/_document.js
@@ -29,17 +29,17 @@ export default class MyDocument extends Document {
           }}
           />
 
-          {/* <script dangerouslySetInnerHTML={{
+          <script dangerouslySetInnerHTML={{
             __html: `var Tawk_API=Tawk_API||{ }, Tawk_LoadStart=new Date();
             (function(){
             var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
             s1.async=true;
-            s1.src='https://embed.tawk.to/XXXXXXXXXXXXXXXXXXXXXXXX/XXXXXXXXX';
+            s1.src='https://embed.tawk.to/61bb5bfa80b2296cfdd20ebb/1fn1tuj3k';
             s1.charset='UTF-8';
             s1.setAttribute('crossorigin','*');
             s0.parentNode.insertBefore(s1,s0);})();`,
           }}
-          /> */}
+          />
 
           <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1446177920850529"
             crossorigin="anonymous"></script>
