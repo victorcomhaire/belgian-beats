a7a649251064fe7dcf0750b0fbdf33317f747ebc
dsantosv <94907533+dsantosv@users.noreply.github.com>
2021-12-16 11:36:43 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,10 +5,10 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          {/* Global Site Tag (gtag.js) - Google Analytics */}
-          {/* <script
+          {* Global Site Tag (gtag.js) - Google Analytics */}
+          {* <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=G-3014JHECCB`}
           />
           <script
             dangerouslySetInnerHTML={{
@@ -16,12 +16,13 @@ export default class MyDocument extends Document {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            gtag('config', 'G-3014JHECCB', {
               page_path: window.location.pathname,
             });
           `
             }}
           /> */}
+          
           {/* <script dangerouslySetInnerHTML={{
             __html: ` (function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('${process.env.NEXT_PUBLIC_LEADFEEDER}');`
           }}
