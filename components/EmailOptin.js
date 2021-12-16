fbe9a44c6e722a27330fab8ddc714abb6f24eb28
dsantosv <94907533+dsantosv@users.noreply.github.com>
2021-12-16 12:29:16 +0100

    Update EmailOptin.js

--- components/EmailOptin.js
+++ components/EmailOptin.js
@@ -10,7 +10,7 @@ const EmailOptin = ({ }) => {
     
     <div className={styles.emailoptin}>
       <div className={styles.optinbutton} onClick={() => toggleformstate()}>Keep me posted through the newsletter</div>
-      {/* {formstate&&<iframe width="540" height="900" src="" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
+      {* {formstate&&<iframe width="540" height="900" src="https://92b83b34.sibforms.com/serve/MUIEAHTEv1_ib7kCBTln9JFhAq5yZMA2hTW-zI-IymVRdCJluH2GrhV7uPxIZ0jDScaNySQi9sLeYF1r-FUXVpD9VpNrAq3uZP3jXCdvubwvKE4EGlWyVihj7GGkENUNx5IE-9gzrT2YM0584AeG17CdqSvH3KApNtte647MNT-zO1o_8ZHR3kry-6Xt2nedNI3S5sPb3ShtYjqQ" frameborder="0" scrolling="auto" allowfullscreen style={{ display: `block`, marginLeft: `auto`, marginRight: `auto`, maxWidth: '100%' }}></iframe>} */}
     </div>
   );
 };
