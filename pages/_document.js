9ff7cb8870e0d13c9cba675f553c0d9301193e35
dsantosv <94907533+dsantosv@users.noreply.github.com>
2021-12-16 11:01:24 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -53,6 +53,10 @@ export default class MyDocument extends Document {
               })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
           </script>
 
+          <script> 
+(function(ss,ex){ window.ldfdr=window.ldfdr||function(){(ldfdr._q=ldfdr._q||[]).push([].slice.call(arguments));}; (function(d,s){ fs=d.getElementsByTagName(s)[0]; function ce(src){ var cs=d.createElement(s); cs.src=src; cs.async=1; fs.parentNode.insertBefore(cs,fs); }; ce('https://sc.lfeeder.com/lftracker_v1_'+ss+(ex?'_'+ex:'')+'.js'); })(document,'script'); })('bElvO73Vw2G8ZMqj'); 
+          </script>
+
         </Head>
         <body>
           <Main />
